c4dc0d735299174a3974df7cb6034b18d639fb28
Apex1429 <apex1429@gmail.com>
2023-02-06 16:29:33 -0500

    Scene-Switch1
    Created logic to have the scene switch from Dock screen to ship list screen. [Win]

---
+++ Mini Space Cargo/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public void ShipList()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+
+}
