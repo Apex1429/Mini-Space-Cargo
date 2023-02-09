c93dedf3700da9addbb4a63dbf79fcd9b3c1cab6
Apex1429 <apex1429@gmail.com>
2023-02-09 17:04:38 -0500

    BUGGED-Spawn-Map-Locations
    Created function to generate locations that will serve as buttons. Currently bugged [Win]

--- Mini Space Cargo/Assets/Scripts/LocationManager.cs
+++ Mini Space Cargo/Assets/Scripts/LocationManager.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using TMPro;
+using UnityEngine.UI;
 
 public class LocationManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+   [Serializable]
+    public struct Locations
+    {
+        public int LocationID;
+        public int SystemID;
+        
+        public string LocationName;
+        public string SystemName;
+        public string DockType;
+        public string IndustryType;
+
+        public Sprite LocationImage;
+
+        public float DistanceFromStar; 
+    } 
+
+    [SerializeField] Locations[] allLocations; 
+
     void Start()
     {
-        
+        GameObject MapSelectorTemplate = transform.GetChild (0).gameObject;
+        GameObject g;
+        int N = allLocations.Length;
+        for (int i = 0; i < N; i++)
+        {
+            g = Instantiate (MapSelectorTemplate, transform);
+            g.transform.GetChild (0).GetComponent <Image> ().sprite = allLocations [i].LocationImage;
+            g.transform.GetChild (1).GetComponent <Text> ().text = allLocations [i].SystemName;
+            g.transform.GetChild (2).GetComponent <Text> ().text = allLocations [i].LocationName;
+            g.transform.GetChild (3).GetComponent <Text> ().text = allLocations [i].DockType;
+            g.transform.GetChild (4).GetComponent <Text> ().text = allLocations [i].IndustryType;
+            g.transform.GetChild (5).GetComponent <Text> ().text = allLocations [i].SystemName;  
+
+            
+        }
+        Destroy (MapSelectorTemplate);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
